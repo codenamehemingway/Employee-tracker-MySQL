e0b679decd3363447f0ea35eb51ef56c12609a42
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-03-15 19:09:38 -0700

    pushig to correct folder. added pseudocode

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
+
+const connection = mysql.createConnection();
+
+connection.connect()
+    firstPrompt();
+
+
+
+// prompts the user for what action they should take
+function firstPrompt()
+
+
+//View Employees/ READ all, SELECT * FROM
+
+
+// Make a department array
+
+
+
+// User choose the department list, employees populate
+
+
+// Make a employee array
+
+
+// Make a employee array to delete
+
+
+function employeeArray() {}
+
+
+
+    //"Add Role" 
