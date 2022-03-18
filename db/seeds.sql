f9d4d864b155ae748c5e86b03bb0c99f2cd0b068
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-03-17 19:42:50 -0700

    adding the seeds.sql info with aadtl js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,39 @@
 USE employeesDB;
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Marketing");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Service");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 70000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Marketing Director", 90000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Marketing Assistant", 80000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 75000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Service Team Lead", 60000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Stan", "Smith", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jodie", "Foster", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Craig", "FromFriday", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jennifer", "Lawerence", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Jones", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Cardi", "B", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Rotch", 1, 2);
--- server.js
+++ server.js
@@ -2,35 +2,56 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
 
-const connection = mysql.createConnection();
+const connection = mysql.createConnection({
+  host: "localhost",
 
-connection.connect()
-    firstPrompt();
+  // Your port; if not 3001
+  port: 3001,
 
+  //username
+  user: "root",
 
+  //password
+  password: "RealEstate2022!",
+  database: "employeesDB",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  console.log(`
+    Employee Manager`);
+
+  firstPrompt();
+});
 
 // prompts the user for what action they should take
-function firstPrompt()
-
+function firstPrompt() {
+  inquirer.prompt({
+    type: "list",
+    name: "task",
+    message: "What would you like to do?",
+    choices: [
+      "view employees",
+      "view employees by dept",
+      "add employees",
+      "remove employee",
+      "add role",
+      "end",
+    ],
+  });
+}
 
 //View Employees/ READ all, SELECT * FROM
 
-
 // Make a department array
-
-
 
 // User choose the department list, employees populate
 
-
 // Make a employee array
-
 
 // Make a employee array to delete
 
-
 function employeeArray() {}
 
-
-
-    //"Add Role" 
+//"Add Role"
