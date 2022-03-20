e697247ec46d59b9ee940dec19bdb782c48957c6
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-03-20 10:50:09 -0700

    continued work on server.js

--- server.js
+++ server.js
@@ -27,29 +27,121 @@ connection.connect(function (err) {
 
 // prompts the user for what action they should take
 function firstPrompt() {
-  inquirer.prompt({
-    type: "list",
-    name: "task",
-    message: "What would you like to do?",
-    choices: [
-      "view employees",
-      "view employees by dept",
-      "add employees",
-      "remove employee",
-      "add role",
-      "end",
-    ],
-  });
+  inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "What would you like to do?",
+      choices: [
+        "view employees",
+        "view employees by dept",
+        "add employees",
+        "remove employee",
+        "add role",
+        "end",
+      ],
+    })
+    .then(function ({ task }) {
+      switch (task) {
+        case "View Employees":
+          viewEmployee();
+          break;
+
+        case "View Employees by Department":
+          viewEmployeeByDepartment();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Remove Employees":
+          removeEmployees();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "End":
+          connection.end();
+          break;
+      }
+    });
 }
 
 //View Employees/ READ all, SELECT * FROM
+function viewEmployee() {
+  console.log("Viewing employees\n");
 
+  var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    console.table(res);
+    console.log("Employees viewed!\n");
+
+    firstPrompt();
+  });
+}
 // Make a department array
+function viewEmployeeByDepartment() {
+  console.log("Viewing employees by department\n");
 
+  var query = `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const departmentChoices = res.map((data) => ({
+      value: data.id,
+      name: data.name,
+    }));
+
+    console.table(res);
+    console.log("Department view succeed!\n");
+
+    promptDepartment(departmentChoices);
+  });
+}
 // User choose the department list, employees populate
 
 // Make a employee array
+function addEmployee() {
+  console.log("Inserting an employee!");
 
+  var query = `SELECT r.id, r.title, r.salary 
+        FROM role r`;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const roleChoices = res.map(({ id, title, salary }) => ({
+      value: id,
+      title: `${title}`,
+      salary: `${salary}`,
+    }));
+
+    console.table(res);
+    console.log("RoleToInsert!");
+
+    promptInsert(roleChoices);
+  });
+}
 // Make a employee array to delete
 
 function employeeArray() {}
